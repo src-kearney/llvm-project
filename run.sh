c93617c9beff43892ae0a53f683a4259623c5607
Sean Kearney <src.kearney@gmail.com>
2025-05-04 16:10:11 -0700

    fix test.mlir

--- run.sh
+++ run.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-MLIR_RUNNER=./build/bin/mlir-cpu-runner
-CUDA_RT_LIB=./build/lib/libmlir_cuda_runtime.so
+BUILD_DIR=build
+MLIR_OPT=$BUILD_DIR/bin/mlir-opt
+MLIR_RUNNER=$BUILD_DIR/bin/mlir-runner
 
-INPUT=lowered.mlir
+$MLIR_OPT test.mlir \
+  --pass-pipeline='gpu-kernel-outlining,gpu-lower-to-nvvm-pipeline' \
+  -o gpu_lowered.mlir
+
+$MLIR_OPT gpu_lowered.mlir \
+  --pass-pipeline='builtin.module(func.func(gpu-async-region,memref-expand,convert-index-to-llvm,finalize-memref-to-llvm,convert-arith-to-llvm,convert-cf-to-llvm,convert-func-to-llvm,gpu-to-llvm,reconcile-unrealized-casts))' \
+  -o lowered.mlir
+
+grep unrealized_conversion_cast lowered.mlir || echo OK
 
 $MLIR_RUNNER \
-  --shared-libs=$CUDA_RT_LIB \
-  --entry-point-result=void \
-  $INPUT
+  -e main --entry-point-result=void \
+  --shared-libs=$BUILD_DIR/lib/libmlir_cuda_runtime.so \
+  --shared-libs=$BUILD_DIR/lib/libmlir_runner_utils.so \
+  lowered.mlir
